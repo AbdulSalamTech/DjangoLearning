350e9771c0b4c2fa6e67632254f85b54522c9e52
abdulsalam <abdulsalam.network@gmail.com>
2024-11-18 19:37:28 +0500

    Added templates

--- project1/app1/views.py
+++ project1/app1/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World By Django.")
+    return render(request, "app1/index.html")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name}!")
+    return render(request, "app1/greet.html", {"name": name.title()})
