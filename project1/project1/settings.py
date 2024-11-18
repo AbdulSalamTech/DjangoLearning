f9f5a77a144a5a187456f35bacc5c081b1188c88
abdulsalam <abdulsalam.network@gmail.com>
2024-11-18 15:50:56 +0500

    Added 2 routes

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     # my apps
-    'hello',
+    'app1',
 ]
 
 MIDDLEWARE = [
